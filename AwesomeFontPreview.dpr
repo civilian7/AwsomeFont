c2be2e6283ef9a73ca04345f8a442bd1ba484775
DELMadang <delmadang95@gmail.com>
2025-06-02 19:50:35 +0900

    fix error

--- AwesomeFontPreview.dpr
+++ AwesomeFontPreview.dpr
@@ -2,8 +2,7 @@ program AwesomeFontPreview;
 
 uses
   Vcl.Forms,
-  Unit3 in 'Unit3.pas' {frmMain},
-  Smart.AwesomeFont in 'Smart.AwesomeFont.pas';
+  uMain in 'uMain.pas' {frmMain};
 
 {$R *.res}
 
